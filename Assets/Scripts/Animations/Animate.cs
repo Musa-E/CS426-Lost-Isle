1128f47b35ccad1ae98d19c8d4f00a2c95c1c75e
Musa-E <melqa2@uic.edu>
2025-04-15 23:05:44 -0500

    Added O2 UI
    Improved O2 UI, and did some side work in some other branches.  For some reason, the rest of this commit seems to be sort of broken.

--- Assets/Scripts/Animations/Animate.cs
+++ Assets/Scripts/Animations/Animate.cs
@@ -1,3 +1,12 @@
+/*
+Filename: Animate.cs
+Author: Musa Elqaq
+Last Edited On: 4/11/2025
+
+Purpose: Automate the animations used on the player in relation to what keys they press.
+*/
+
+using System;
 using UnityEngine;
 
 public class Animate : MonoBehaviour {
@@ -13,5 +22,24 @@ public class Animate : MonoBehaviour {
             // Set the trigger value to True for the parameter Dance.
             anim.SetTrigger("Jump");
         }
+
+        // Set the trigger value to True for Walking if the player presses the "w" key
+        if (Input.GetKeyDown(KeyCode.W)) {
+            anim.SetTrigger("Walk");
+
+            // Set the trigger value to True for Sprinting if player is both walking and pressing shift
+            if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) {
+                anim.SetTrigger("Sprint");
+            }
+        }
+    }
+
+    // todo
+    /// <summary>
+    /// When true, the player is currently on a stair case, so the stair animation(s) should be used
+    /// </summary>
+    /// <returns>True or False - True if on a staircase, false if not</returns>
+    Boolean isOnStairs() {
+        return false;
     }
 }
