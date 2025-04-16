1934e16617fa64a4af1eaec2a4182b23c84e4373
Musa-E <melqa2@uic.edu>
2025-04-15 23:15:50 -0500

    Updated to match Karthik's branch
    There were some issues with my branch, so this should hopefully sync it with Karthik's.

--- Assets/Scripts/OxygenCounter.cs
+++ Assets/Scripts/OxygenCounter.cs
@@ -1,12 +1,9 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class OxygenCounter : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI oxygenLevelText;
-
-    public Slider slider;
 
     public int oxygenLevel = 100;
     public float RateOfO2Reduction = 1f; 
@@ -16,9 +13,6 @@ public class OxygenCounter : MonoBehaviour
     private void Start()
     {
         UpdateOxygenLevelText(oxygenLevel);
-        
-        // Update the O2 UI
-        setMaxOxygen(100);
     }
 
     private void Update()
@@ -45,9 +39,6 @@ public class OxygenCounter : MonoBehaviour
 
     private int O2LevelReduction(int currentO2Level, int RateOfO2Reduction)
     {
-        // Update O2 UI
-        setOxygen(currentO2Level - RateOfO2Reduction);
-        
         return currentO2Level - RateOfO2Reduction;
     }
     private void UpdateOxygenLevelText(int currentO2Level)
@@ -55,13 +46,4 @@ public class OxygenCounter : MonoBehaviour
         oxygenLevelText.text = $"Oxygen: {currentO2Level}";
     }
     
-
-    public void setOxygen(int oxygenLevel) {
-        slider.value = oxygenLevel;
-    }
-
-    public void setMaxOxygen(int maxOxygen) {
-        slider.maxValue = maxOxygen;
-        slider.value = oxygenLevel;
-    }
 }
